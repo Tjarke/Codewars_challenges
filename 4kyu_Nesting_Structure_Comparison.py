1c00c162dec449f995b8d032fb215081139cb8f7
Tjarke <Tjarke@web.de>
2021-08-16 20:32:06 +0200

    added solution

---
+++ 4kyu_Nesting_Structure_Comparison.py
@@ -0,0 +1,36 @@
+"""
+Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+
+For example:
+
+# should return True
+same_structure_as([ 1, 1, 1 ], [ 2, 2, 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ 2, [ 2, 2 ] ] )
+
+# should return False 
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2 ], 2 ] )
+
+# should return True
+same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
+
+# should return False
+same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )
+
+"""
+
+
+def same_structure_as(original, other):
+    return structure(original) == structure(other)
+
+
+def structure(original):
+    if not isinstance(original, list):
+        return False
+    ans = [len(original)]
+    for i in original:
+        if isinstance(i, list):
+            ans.append(structure(i))
+        else:
+            ans.append(1)
+    return ans
