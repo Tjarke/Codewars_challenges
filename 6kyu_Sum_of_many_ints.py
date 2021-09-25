47861b7ff78553bbf4ef96afb0c4baf38c49fa3f
Tjarke <Tjarke@web.de>
2021-09-25 23:54:00 +0200

    added solution

---
+++ 6kyu_Sum_of_many_ints.py
@@ -0,0 +1,17 @@
+"""
+Write this function
+
+f(n,m) = sum i to n  from ( i%m )
+
+for i from 1 to n, do i % m and return the sum
+
+f(n=10, m=5) // returns 20 (1+2+3+4+0 + 1+2+3+4+0)
+
+You'll need to get a little clever with performance, since n can be a very large number
+
+
+"""
+
+
+def f(n, m):
+  #My function body is ready
